bfc2964c9df8c0224cd3f3ffa9f342625d648049
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2026-02-07 12:00:06 -0500

    Add files via upload

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -12,34 +12,41 @@ current_llvm_stable() {
 install_llvm() {
   llvm_version="$1";
   version="${llvm_version}";
-  tmpdir="$(mktemp -d)";
-  cd "${tmpdir}";
+  tmp_dir="$(mktemp -d)";
+  cd "${tmp_dir}";
   curl -fsSL https://apt.llvm.org/llvm.sh -o llvm.sh
   chmod +x llvm.sh
   sudo env DPKG_FORCE=overwrite ./llvm.sh "${llvm_version}" all
-  rm -rf -- "${tmpdir}";
-  tmpfile="$(mktemp)";
-  echo "/usr/lib/llvm-$llvm_version/bin" >>"$tmpfile";
-  cat "${GITHUB_PATH}" >>"${tmpfile}";
-  cat "${tmpfile}" > "${GITHUB_PATH}";
-  rm -f -- "${tmpfile}";
-  PATH="/usr/lib/llvm-${llvm_version}/bin:${PATH}";
+  rm -rf -- "${tmp_dir}";
+  tmp_file="$(mktemp)";
+  echo "/usr/lib/llvm-${llvm_version}/bin" >>"${tmp_file}";
+  cat "${GITHUB_PATH}" >>"${tmp_file}";
+  cat "${tmp_file}" > "${GITHUB_PATH}";
+  rm -f -- "${tmp_file}";
   LLVM_PATH="/usr/lib/llvm-${llvm_version}";
-  export PATH LLVM_PATH version
+  PATH="${LLVM_PATH}/bin:${PATH}";  
+  export LLVM_PATH PATH version
 }
 
-sanity_check() {
+llvm_version() {
   llvm_version="$(llvm-config --version)";
-  if [[ "${llvm_version}" != "${version%%.*}" ]]; then
-    echo "Expected LLVM major version ${version}, got ${llvm_version}" >&2
+  if [[ $(printf '%s\n%s' "${llvm_version}" "${version}" | sort -V | head -n1) == "${llvm_version}" ]] && [[ "${llvm_version}" != "${version}" ]]; then
+    echo "Expected LLVM major version: ${version}, but got default version: ${llvm_version}" >&2
     exit 0
+  elif [[ "${llvm_version}" == "${version}" ]]; then
+    echo "Updated LLVM major version: ${llvm_version}" 2>&1;
+  elif [[ "${llvm_version%%.*}" != "${version}" ]]; then
+    echo "Expected LLVM major version: ${version}, but got version: ${llvm_version}" >&2
+    exit 0
+  else
+    echo "Updated LLVM major version: ${llvm_version}" 2>&1;
   fi;
 }
 
 LLVM_VERSION="${LLVM_VERSION:-$(current_llvm_stable)}";
 install_llvm "${LLVM_VERSION}" >/dev/null 2>&1;
-sanity_check "${LLVM_VERSION}" >/dev/null;
+llvm_version "${LLVM_VERSION}" >/dev/null;
 
-echo -e "\nLLVM ${LLVM_VERSION} has been installed to ${LLVM_PATH}" 2>&1;
-echo "PATH=${PATH}" >>"${GITHUB_PATH}";
-echo "LLVM_PATH=${LLVM_PATH}" >>"${GITHUB_ENV}";
+echo -e "\n\033[32mLLVM ${LLVM_VERSION} has been installed to\033[0m ${LLVM_PATH}" 2>&1;
+echo "PATH=${PATH}" >> "${GITHUB_PATH}";
+echo "LLVM_PATH=${LLVM_PATH}" >> "${GITHUB_ENV}";
