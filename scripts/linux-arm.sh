caae98187bb44de00309f30c1f13be3c9c70f9d9
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2026-02-16 13:01:34 -0500

    Add files via upload

--- scripts/linux-arm.sh
+++ scripts/linux-arm.sh
@@ -128,7 +128,7 @@ llvm_arm_install() {
     echo "Toolchain: ${LLVM_ARM_TOOLCHAIN}";
     sudo dpkg --add-architecture i386 2>/dev/null
     sudo apt-get -yq update >/dev/null
-    sudo aptitude install -yq gcc-arm-linux-gnueabihf gcc-multilib libc6-dev libgcc-s1 libc6:i386 libstdc++6:i386 >/dev/null 2>&1
+    sudo aptitude install -yq sudo apt-get install -y linux-headers-generic-armhf gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf libc6-dev-armhf-cross gcc-multilib libc6-dev libgcc-s1 libc6:i386 libstdc++6:i386 gcc-aarch64-linux-gnu g++-aarch64-linux-gnu libc6-dev-arm64-cross >/dev/null 2>&1
     echo -e "Compilier: ${check1:-${check2:-$check3}}";
     echo -e "\nLibrary Path: $LIBRARY_PATH";
     echo -e "\n\033[32mllvm-embedded-toolchain-for-arm-\033[0m${version}\033[32m has been installed to\033[0m \033[1m${LLVM_ARM_TOOLCHAIN}\033[0m.";
